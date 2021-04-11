7ce8137abc5b2cb5f6761479bf005c405a4f3fef
Gerard Mangues <gerardcoding@gmail.com>
2021-04-11 12:44:04 +0200

    Add finssihed mobile and desktop versions of header component

--- frontend/src/app/search-bar/search-bar.component.ts
+++ frontend/src/app/search-bar/search-bar.component.ts
@@ -7,9 +7,6 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarComponent implements OnInit {
 
-  expand():void{
-    document.querySelector('input')?.classList.add('expanded');
-  }
   constructor() { }
 
   ngOnInit(): void {
