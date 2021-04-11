bab7f0b4d67d95a0c0e0b20f0922e0d6bfad58fd
Gerard Mangues <gerardcoding@gmail.com>
2021-04-11 12:37:51 +0200

    Add search bar component finnished

--- frontend/src/app/search-bar/search-bar.component.ts
+++ frontend/src/app/search-bar/search-bar.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarComponent implements OnInit {
 
+  expand():void{
+    document.querySelector('input')?.classList.add('expanded');
+  }
   constructor() { }
 
   ngOnInit(): void {
