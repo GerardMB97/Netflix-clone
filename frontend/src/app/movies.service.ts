f5e5e31fe4f9ce9ea83025821c6423acc9d4daae
Gerard Mangues <gerardcoding@gmail.com>
2021-04-27 22:17:18 +0200

    Add 2 private properties to movies service - API_KEY - ENDPOINT

--- frontend/src/app/movies.service.ts
+++ frontend/src/app/movies.service.ts
@@ -7,9 +7,9 @@ import { HttpClient } from '@angular/common/http'
 })
 export class MoviesService {
 
-  private apiKey = 'a3da1cfa'
+  private API_KEY = '74662e7f8d60e0865f25e6d5394a6720'
 
-  private apiUrl = 'http://www.omdbapi.com/'
+  private endPoint = 'https://api.themoviedb.org/3'
 
   title$ = new Subject
 
