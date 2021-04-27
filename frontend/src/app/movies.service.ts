05d6e2e885b58a9cfb3b4f74eb6cb1d7babae371
Gerard Mangues <gerardcoding@gmail.com>
2021-04-27 22:43:31 +0200

    Add a private method to handleErrors

--- frontend/src/app/movies.service.ts
+++ frontend/src/app/movies.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
-import { HttpClient } from '@angular/common/http'
+import { Observable, Subject, throwError } from 'rxjs';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +9,25 @@ export class MoviesService {
 
   private API_KEY = '74662e7f8d60e0865f25e6d5394a6720'
 
-  private endPoint = 'https://api.themoviedb.org/3'
+  private ENDPOINT = 'https://api.themoviedb.org/3'
+
+  private GET_POPULAR = `${this.ENDPOINT}/movie/popular`
+
+  private params = new HttpParams().append('api_key', this.API_KEY)
 
   title$ = new Subject
 
   constructor(private http: HttpClient) { }
+
+    private handleError(error: HttpErrorResponse):Observable<Error> {
+        let errorMessage = '';
+        if (error.error instanceof ErrorEvent) {
+            // client-side error
+            errorMessage = `Error: ${error.error.message}`;
+        } else {
+            // server-side error
+            errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+        }
+        return throwError(errorMessage);
+    }
 }
