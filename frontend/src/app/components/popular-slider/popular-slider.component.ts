24bdf543a1626bf0c628cc4760f37c045e82a7de
Gerard Mangues <gerardcoding@gmail.com>
2021-04-28 16:05:42 +0200

    Add an ngFor that loops over the 10 most popular movies

--- frontend/src/app/components/popular-slider/popular-slider.component.ts
+++ frontend/src/app/components/popular-slider/popular-slider.component.ts
@@ -9,14 +9,11 @@ import { MoviesService } from 'src/app/services/movies.service';
   styleUrls: ['./popular-slider.component.scss']
 })
 export class PopularSliderComponent implements OnInit {
+  popularRankingPositions = new Array(5).fill(1).map((x,i)=> i+1)
 
   movies$: Observable<Movie[]> | undefined
-  constructor(private moviesService: MoviesService) { }
 
-  debug(){
-    console.log(this.movies$)
-    console.log(this.moviesService.movies$)
-  }
+  constructor(private moviesService: MoviesService) { }
   ngOnInit(): void {
     this.movies$ = this.moviesService.movies$
     this.moviesService.getPopularMovies().subscribe()
